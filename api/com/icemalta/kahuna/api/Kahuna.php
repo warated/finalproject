37e5ebb1b6723fb8d68180ea4e9154d4cf62dec1
Keith Vassallo <keith@icemalta.com>
2024-02-11 21:40:36 +0000

    Added CORS handling to API handler.

--- api/com/icemalta/kahuna/api/Kahuna.php
+++ api/com/icemalta/kahuna/api/Kahuna.php
@@ -2,6 +2,7 @@
 require 'com/icemalta/kahuna/util/ApiUtil.php';
 use com\icemalta\kahuna\util\ApiUtil;
 
+cors();
 
 $endPoints = [];
 $requestData = [];
@@ -67,6 +68,31 @@ $endpoints["/"] = function (string $requestMethod, array $requestData): void {
 $endpoints["404"] = function (string $requestMethod, array $requestData): void {
     sendResponse(null, 404, "Endpoint " . $requestData["endPoint"] . " not found.");
 };
+
+function cors()
+{
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PATCH, DELETE");
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+}
 
 try {
     if (isset($endpoints[$endPoint])) {
